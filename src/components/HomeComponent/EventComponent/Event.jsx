4889cfd9d8ee4e0d9f71193985b9047b1802ae90
Istiak Ahmed <istiakahmed104127@gmail.com>
2022-09-25 03:50:29 +0600

    little change

--- src/components/HomeComponent/EventComponent/Event.jsx
+++ src/components/HomeComponent/EventComponent/Event.jsx
@@ -4,31 +4,31 @@ import "./Event.css";
 const Event = () => {
   return (
     <div id="cards_landscape_wrap-2">
-      <div class="container">
-        <div className="">
+      <div className="container">
+        <div>
           <h2 className="text-center fw-bold">
             ঈশ্বর আপনাকে যা দিয়েছেন তা থেকে মানুষকে দিন। এটি অবশ্যই বৃহত্তর মান
             সহ আপনার কাছে ফিরে আসবে
           </h2>
-          <p>
+          <p style={{ color: "#ed0404" }}>
             রক্তবন্ধু কোন একক সংগঠনের জন্য নয়, বরং সকল ব্যক্তির, সংগঠনের সুবিধার
             জন্যই। যাঁরা রক্তদেন তাঁদেরকে এবং রক্তদান সম্পর্কিত বিভিন্ন
             সংগঠনগুলোকে এক প্লাটফর্মে নিয়ে এসে রক্ত দেওয়া-পাওয়ার কাজটা সহজ করাই
             এর উদ্দেশ্য। আপনিও রক্তদাতা হলে website এ রেজিস্ট্রেশন করুন।
           </p>
         </div>
-        <div class="row">
-          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
             <a href="/">
-              <div class="card-flyer">
-                <div class="text-box">
-                  <div class="image-box">
+              <div className="card-flyer">
+                <div className="text-box">
+                  <div className="image-box">
                     <img
                       src="https://wp.bssnews.net/wp-content/uploads/2021/06/World-Blood-Donor-Day.png"
                       alt=""
                     />
                   </div>
-                  <div class="text-container text-start">
+                  <div className="text-container text-start">
                     <h6>
                       মোবাইলে এক ক্লিকেই মিলবে রক্ত! নয়া উদ্যোগ রক্তবন্ধু
                       ক্লাবের
@@ -40,7 +40,10 @@ const Event = () => {
                       পর্যন্ত ২.০৭ লক্ষেরও বেশি রক্তদাতা নিবন্ধিত হয়েছে।
                     </p>
                     <Link to="/">
-                      <button type="button" class="btn btn-outline-danger mt-3">
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger mt-3"
+                      >
                         বিস্তারিত
                       </button>
                     </Link>
@@ -49,17 +52,17 @@ const Event = () => {
               </div>
             </a>
           </div>
-          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
             <a href="/">
-              <div class="card-flyer">
-                <div class="text-box">
-                  <div class="image-box">
+              <div className="card-flyer">
+                <div className="text-box">
+                  <div className="image-box">
                     <img
                       src="https://wp.bssnews.net/wp-content/uploads/2021/06/World-Blood-Donor-Day.png"
                       alt=""
                     />
                   </div>
-                  <div class="text-container text-start">
+                  <div className="text-container text-start">
                     <h6>
                       মোবাইলে এক ক্লিকেই মিলবে রক্ত! নয়া উদ্যোগ রক্তবন্ধু
                       ক্লাবের
@@ -71,7 +74,10 @@ const Event = () => {
                       পর্যন্ত ২.০৭ লক্ষেরও বেশি রক্তদাতা নিবন্ধিত হয়েছে।
                     </p>
                     <Link to="/">
-                      <button type="button" class="btn btn-outline-danger mt-3">
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger mt-3"
+                      >
                         বিস্তারিত
                       </button>
                     </Link>
@@ -80,17 +86,17 @@ const Event = () => {
               </div>
             </a>
           </div>
-          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
             <a href="/">
-              <div class="card-flyer">
-                <div class="text-box">
-                  <div class="image-box">
+              <div className="card-flyer">
+                <div className="text-box">
+                  <div className="image-box">
                     <img
                       src="https://wp.bssnews.net/wp-content/uploads/2021/06/World-Blood-Donor-Day.png"
                       alt=""
                     />
                   </div>
-                  <div class="text-container text-start">
+                  <div className="text-container text-start">
                     <h6>
                       মোবাইলে এক ক্লিকেই মিলবে রক্ত! নয়া উদ্যোগ রক্তবন্ধু
                       ক্লাবের
@@ -102,7 +108,10 @@ const Event = () => {
                       পর্যন্ত ২.০৭ লক্ষেরও বেশি রক্তদাতা নিবন্ধিত হয়েছে।
                     </p>
                     <Link to="/">
-                      <button type="button" class="btn btn-outline-danger mt-3">
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger mt-3"
+                      >
                         বিস্তারিত
                       </button>
                     </Link>
@@ -111,17 +120,17 @@ const Event = () => {
               </div>
             </a>
           </div>
-          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
             <a href="/">
-              <div class="card-flyer">
-                <div class="text-box">
-                  <div class="image-box">
+              <div className="card-flyer">
+                <div className="text-box">
+                  <div className="image-box">
                     <img
                       src="https://wp.bssnews.net/wp-content/uploads/2021/06/World-Blood-Donor-Day.png"
                       alt=""
                     />
                   </div>
-                  <div class="text-container text-start">
+                  <div className="text-container text-start">
                     <h6>
                       মোবাইলে এক ক্লিকেই মিলবে রক্ত! নয়া উদ্যোগ রক্তবন্ধু
                       ক্লাবের
@@ -133,7 +142,10 @@ const Event = () => {
                       পর্যন্ত ২.০৭ লক্ষেরও বেশি রক্তদাতা নিবন্ধিত হয়েছে।
                     </p>
                     <Link to="/">
-                      <button type="button" class="btn btn-outline-danger mt-3">
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger mt-3"
+                      >
                         বিস্তারিত
                       </button>
                     </Link>
